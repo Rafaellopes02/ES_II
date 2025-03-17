691f64ab0956121c4f6d0cd6df2282c4d144c148
Tiagomelo03 <tiagomelo@ipvc.pt>
2025-03-17 18:10:46 +0000

    Ligação a bd

---
+++ TrabalhoESII/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrabalhoESII.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Exemplo de tabela
+        public DbSet<Exemplo> Exemplos { get; set; }
+    }
+
+    public class Exemplo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ TrabalhoESII/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using TrabalhoESII.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar o DbContext com PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
