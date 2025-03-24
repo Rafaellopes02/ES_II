002f94459703c0ef4bff01af4163957a4e70abff
Daniel Trindade <daniptrindade3@gmail.com>
2025-03-24 17:43:15 +0000

    Adição gestão contas

--- TrabalhoESII/Controllers/AccountController.cs
+++ TrabalhoESII/Controllers/AccountController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TrabalhoESII.Models;
 
 namespace TrabalhoESII.Controllers
 {
     public class AccountController : Controller
     {
+        
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         [HttpGet("login")]
         public IActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string nomeUtilizador, string senha)
+        {
+            var user = _context.utilizadores.FirstOrDefault(u => u.nomeutilizador == nomeUtilizador && u.senha == senha);
+            if (user != null)
+            {
+                return RedirectToAction("Index", "Dashboard", new { userType = user.tiposutilizadores.nome });
+            }
+            ViewBag.ErrorMessage = "Credenciais inválidas.";
             return View();
         }
         [HttpGet("register")]
