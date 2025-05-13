b84a53bb0683683ca0ee2473e9b8966d46800280
Daniel Trindade <daniptrindade3@gmail.com>
2025-05-13 16:50:08 +0100

    Add notification system with dropdown and API integration

---
+++ TrabalhoESII/Controllers/NotificacoesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrabalhoESII.Models;
+
+namespace TrabalhoESII.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class NotificacoesController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public NotificacoesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetNotificacoes()
+    {
+        var userId = int.Parse(User.FindFirst("UserId").Value);
+        
+        var notificacoes = await _context.notificacoes
+            .Where(n => n.idutilizador == userId)
+            .OrderByDescending(n => n.idnotificacao)
+            .Select(n => new {
+                n.idnotificacao,
+                n.mensagem,
+            })
+            .ToListAsync();
+
+        return Ok(notificacoes);
+    }
+}
